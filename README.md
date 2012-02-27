3a03b609749768c92d1042fd0cc8239a702750ed
Andy <andy.farrell@phgroup.com>
2012-02-27 11:06:32 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# NoiseTex
+
+A tiny (729 bytes minified) function that generates a block of noise of your chosen size
+and opacity, then uses it as the background image for an element you specify.
+Also returns a DataURL string so you can use the generated image on other
+elements.
+
+## Usage
+
+Simply call the `addNoise` function:
+
+    addNoise(document.body, 200, 0.05);
+
+The first parameter is the element to which you want to apply the noise texture.
+The second is the size of noise block to generate &ndash; larger blocks will
+show less obvious repetition, but take up a bit more memory and make your HTML
+element inspector more awkward to use due to the giant inline dataURL. The last
+parameter is the opacity; the value in the example results in a nice subtle
+texture that looks a bit like paper. Values over 0.2 will be very obvious.
+Maximum value is 1.
+
+## Globalness
+
+The function is presented as a global variable, which is generally not ideal for
+non-trivial web sites/applications. If you&rsquo;re using some means of avoiding
+this, such as namespacing, it&rsquo; very easy to adapt the function to whatever
+system you&rsquo;re using.
---
+++ noisetex.js
@@ -0,0 +1,57 @@
+var addNoise;
+
+if (typeof addNoise !== "function") {
+    addNoise = function (element, size, alpha) {
+        // Renders a noise block of the given size then sets it as the given
+        // element's background image. Very low alpha values (< 0.2) are
+        // recommended to stop the texture being too overpowering.
+
+        // Example:
+        // addNoise(document.body, 200, 0.04);
+
+        "use strict";
+
+        var noiseCanvas = document.createElement("canvas"),
+            noiseCtx,
+            pixelData, pixels,
+            x, y, sx, sy,
+            rand;
+
+        size = size || 100;
+        alpha = alpha || 0.1;
+        if (alpha > 1.0) {
+            alpha = 1.0;
+        }
+
+        // Bail out if browser doesn't support canvas functions.
+        if (!noiseCanvas.getContext || !element || !element.style) {
+            return;
+        }
+
+        noiseCanvas.width = noiseCanvas.height = size;
+        noiseCtx = noiseCanvas.getContext("2d");
+
+        pixelData = noiseCtx.getImageData(0, 0, size, size);
+        pixels = pixelData.data;
+
+        // Render a monochrome black noise block
+        for (x = 0, sx = pixels.length; x < sx; x += 4) {
+            // First 3 numbers are R, G and B.
+            pixels[x] = pixels[x + 1] = pixels[x + 2] = 0;
+
+            // 4th number is alpha.
+            rand = Math.random();
+            // Binary OR is slightly faster than Math.round()
+            pixels[x + 3] = (255 * rand * rand * alpha) | 0;
+        }
+
+        noiseCtx.putImageData(pixelData, 0, 0);
+
+        pixelData = "url(" + noiseCanvas.toDataURL() + ")";
+
+        // Beware: takes up lots of space in Firebug (etc.) element view ;)
+        element.style.backgroundImage = pixelData;
+
+        return pixelData;
+    };
+}
